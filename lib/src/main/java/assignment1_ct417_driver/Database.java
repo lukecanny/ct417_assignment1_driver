8287ad8c06ff7a82f11a861d5e52d64aa51975b3
lukecanny <lukecanny@protonmail.ch>
2022-10-06 17:48:26 +0100

    Added Github Repository as dependency in build.gradle

--- lib/src/main/java/assignment1_ct417_driver/Database.java
+++ lib/src/main/java/assignment1_ct417_driver/Database.java
@@ -1,5 +1,18 @@
 package assignment1_ct417_driver;
 
-public class Database {
+import assignment1_ct417.*;
+import org.joda.time.*;
 
+public class Database {
+	
+	Lecturer johndoe = new Lecturer("John Doe", new LocalDate(1955,1,5));
+	
+	
+	Database() {
+		
+		
+	}
+	public static void main (String[] args) {
+		
+	}
 }
